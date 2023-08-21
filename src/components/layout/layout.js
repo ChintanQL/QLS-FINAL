9cbdc986ad20e5b7fe9530110583a8e6a8c7e501
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2023-08-21 13:11:22 +0530

    Update layout.js

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import Footer from "../footer/footer"
 import '../../stylee.css'
 import '../../responsive.css'
-import NavBar from "../Header/nav-bar"
+import NavBar from "../Header/NavBar"
 
 
 const Layout = ({ children }) => (
