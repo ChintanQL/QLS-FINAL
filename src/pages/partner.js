d443e489af9181fbb83bdf999d5259c09eb0375c
amit371996 <111500321+amit371996@users.noreply.github.com>
2023-08-25 02:03:45 -0700

    Update partner.js

--- src/pages/partner.js
+++ src/pages/partner.js
@@ -194,7 +194,9 @@ export default function Partner() {
                                         })}
 
 
-                                        <ul className="pagination">{renderPageNumbers()}</ul>
+                                      {totalItems > itemsPerPage && (
+                                            <ul className="pagination">{renderPageNumbers()}</ul>
+                                        )}
 
                                     </div>
                                 </div>
