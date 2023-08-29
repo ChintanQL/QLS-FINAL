7594279f5730dd12ddaf90b4c7339c97c0fcab01
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2023-08-29 18:27:07 +0530

    Update news.js

--- src/pages/news.js
+++ src/pages/news.js
@@ -124,7 +124,7 @@ export default function Whatsnew() {
 												</div>
 												<div className="img_arrow">
 													<div className="roww_wrap">
-														<a href="#"><img src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/arrow.svg" /></a>
+														<a href="#"><img src="https://admin.qlspace.com.au/wp-content/themes/qls/assets/image/arrow.svg" /></a>
 													</div>
 												</div>
 											</div>
@@ -161,16 +161,16 @@ export default function Whatsnew() {
 														<div className="our_lkhb">
 															<div className="imh_bc">
 																<div className="inner_ghv">
-																	<a href="https://www.qlspace.com.au/news/spacex-successfully-launches-10-space-force-tranche-0-satellites-after-abort-delay-lands-rocket/">
+																	<a href="https://admin.qlspace.com.au/news/spacex-successfully-launches-10-space-force-tranche-0-satellites-after-abort-delay-lands-rocket/">
 																		<img src={news.news.archiveImage.sourceUrl} className='img-fluid' />
 																	</a>
 																</div>
 																<div className="date_wrad_news">
 																	<div className="date_rn">
-																		<div className="date_icon"><span><img src="https://www.qlspace.com.au/wp-content/uploads/2023/03/icon-_calendar-outline_.png" /></span>{news.featuredImage.node.date}</div>
+																		<div className="date_icon"><span><img src="https://admin.qlspace.com.au/wp-content/uploads/2023/03/icon-_calendar-outline_.png" /></span>{news.featuredImage.node.date}</div>
 																	</div>
 																	<div className="oir_amin">
-																		<div className="uhgbv"><span><img src="https://www.qlspace.com.au/wp-content/uploads/2023/03/userfv.png" /></span>{news.featuredImage.node.author && news.featuredImage.node.author.node && news.featuredImage.node.author.node.name}</div>
+																		<div className="uhgbv"><span><img src="https://admin.qlspace.com.au/wp-content/uploads/2023/03/userfv.png" /></span>{news.featuredImage.node.author && news.featuredImage.node.author.node && news.featuredImage.node.author.node.name}</div>
 																	</div>
 																</div>
 																<div className="heading_nmb">
@@ -181,7 +181,7 @@ export default function Whatsnew() {
 																	</h4>
 																	<div className="next_page">
 																		<Link to={"/news/" + news.slug} className="read-more"> Read more
-																			<span><img src="https://www.qlspace.com.au/wp-content/uploads/2023/03/arrowright.png" /></span>
+																			<span><img src="https://admin.qlspace.com.au/wp-content/uploads/2023/03/arrowright.png" /></span>
 																		</Link>
 																	</div>
 																</div>
