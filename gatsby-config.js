cd1965efceed814b58858b1aa4b40eeb4f6d08c4
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2023-08-29 17:21:32 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // siteUrl: `https://www.qlspace.com.au/graphql`,
-    siteUrl: `https://www.qlspace.com.au/graphql`,
+    siteUrl: `https://admin.qlspace.com.au/graphql`,
   },
   plugins: [
     {
