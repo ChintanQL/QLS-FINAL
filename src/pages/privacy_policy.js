433a93475601454bcc151a5fd54a1feb5395d733
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2023-08-29 18:28:01 +0530

    Update privacy_policy.js

--- src/pages/privacy_policy.js
+++ src/pages/privacy_policy.js
@@ -22,7 +22,7 @@ export default function Privacy_Policy() {
                                     </div>
                                     <div className="img_arrow">
                                         <div className="roww_wrap">
-                                            <a href="#"><img src="https://www.qlspace.com.au/wp-content/themes/qls/assets/image/arrow.svg" /></a>
+                                            <a href="#"><img src="https://admin.qlspace.com.au/wp-content/themes/qls/assets/image/arrow.svg" /></a>
                                         </div>
                                     </div>
                                 </div>
