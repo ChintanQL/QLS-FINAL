108404cc5ef2f1e7f739a7a7b0dd17eac5580b8b
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2025-04-02 09:23:41 +0530

    Create LinkedInFeed.js

---
+++ src/components/Header/LinkedInFeed.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+
+const LinkedInFeed = () => {
+  useEffect(() => {
+    // Dynamically add the script
+    const script = document.createElement('script');
+    script.src = 'https://static.elfsight.com/platform/platform.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      // Clean up the script when the component is unmounted
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className="elfsight-app-24c802da-3ea0-4fad-b5d4-3e16bccb0fa2" data-elfsight-app-lazy></div>
+  );
+};
+
+export default LinkedInFeed;
