5aab1a2f3349c803361c35314260aed3f9afb23a
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2025-04-02 09:52:33 +0530

    Update LinkedInFeed.js

--- src/components/Header/LinkedInFeed.js
+++ src/components/Header/LinkedInFeed.js
@@ -2,25 +2,17 @@ import React, { useEffect } from 'react';
 
 const LinkedInFeed = () => {
   useEffect(() => {
-   
-    const script = document.createElement('script');
-    script.src = 'https://static.elfsight.com/platform/platform.js';
-    script.async = true;
-    document.body.appendChild(script);
-    sonsole.log(script);
-  
-    return () => {
-    
-      document.body.removeChild(script);
-    };
+    // Wait until the script is loaded, and then initialize the widget
+    if (window.Elfsight) {
+      window.Elfsight.init();
+    }
   }, []);
 
   return (
-    <>
-    <div>asasasasasasasas</div>
-    <div className="elfsight-app-24c802da-3ea0-4fad-b5d4-3e16bccb0fa2" data-elfsight-app-lazy></div>
-    </>
-    
+    <div
+      className="elfsight-app-24c802da-3ea0-4fad-b5d4-3e16bccb0fa2"
+      data-elfsight-app-lazy
+    />
   );
 };
 
